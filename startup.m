409dd0f208b762122d3e61e117f10bd4fec5c57c
xiulinwang <43029713+xiulinwang@users.noreply.github.com>
2021-11-18 19:15:18 +0800

    Delete startup.m

--- startup.m
+++
@@ -1,9 +0,0 @@
-function startup
-%filename='/Users/xiulinwang/Documents/MATLAB/Toolbox'; % mac
-filename='E:\Project\Toolbox';% win
-%filename='/media/cao/9ADE97E1DE97B44B/WangXiulin/toolbox'; % server-ubuntu
-%filename='D:\WangXiulin\toolbox'; % server-win
-addpath(genpath(filename));
-end
-
-
